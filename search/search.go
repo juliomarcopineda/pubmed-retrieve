51200edb0342b0d1d131b03fa341488d865908ff
juliomarcopineda <juliomarcopineda@gmail.com>
2020-04-20 20:01:49 -0700

    change to decoder

--- search/search.go
+++ search/search.go
@@ -3,7 +3,7 @@ package search
 import (
 	"encoding/xml"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"net/url"
 )
@@ -39,32 +39,30 @@ func GetPmids(query string) ([]int, error) {
 	}
 	esearchURL.RawQuery = esearchQuery.Encode()
 
-	xmlString, err := getXML(esearchURL.String())
+	body, err := getXML(esearchURL.String())
 	if err != nil {
-		return nil, fmt.Errorf("Failed to get XML: %v", err)
+		return nil, fmt.Errorf("Failed to get XML Body: %v", err)
 	}
 
 	var result eSearchResult
-	xml.Unmarshal([]byte(xmlString), &result)
+	err = xml.NewDecoder(body).Decode(&result)
+	if err != nil {
+		return nil, fmt.Errorf("Read body: %v", err)
+	}
 
+	body.Close()
 	return result.Pmids.PmidSlice, nil
 }
 
-// getXML returns the XML body of a given URL string
-func getXML(url string) (string, error) {
+// getXML returns the XML body of a given URL string as an io.ReadCloser
+func getXML(url string) (io.ReadCloser, error) {
 	response, err := http.Get(url)
 	if err != nil {
-		return "", fmt.Errorf("GET error: %v", err)
+		return nil, fmt.Errorf("GET error: %v", err)
 	}
 	if response.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("Status error: %v", response.StatusCode)
+		return nil, fmt.Errorf("Status error: %v", response.StatusCode)
 	}
 
-	data, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return "", fmt.Errorf("Read body: %v", err)
-	}
-
-	response.Body.Close()
-	return string(data), nil
+	return response.Body, nil
 }
