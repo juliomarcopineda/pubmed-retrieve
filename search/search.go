949f693277b57f8d5a55eebdee71f186b25f3dd5
juliomarcopineda <juliomarcopineda@gmail.com>
2020-04-20 18:57:13 -0700

    function to retrieve XML string from query

--- search/search.go
+++ search/search.go
@@ -1,17 +1,60 @@
 package search
 
-var esearch string = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
 
-type Query struct {
-}
+var esearchURLString string = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?"
 
 // GetPmids ...
-func GetPmids(query string) []int {
-	pmids := make([]int, 3)
+func GetPmids(query string) ([]int, error) {
+	pmidQuery := map[string]string{
+		"db":     "pubmed",
+		"term":   query,
+		"retmax": "100000",
+	}
 
-	return pmids
+	esearchURL, err := url.Parse(esearchURLString)
+	if err != nil {
+		return nil, fmt.Errorf("URL Parse Error: %v", err)
+	}
+
+	esearchQuery := esearchURL.Query()
+	for key, val := range pmidQuery {
+		esearchQuery.Set(key, val)
+	}
+	esearchURL.RawQuery = esearchQuery.Encode()
+
+	xmlString, err := getXML(esearchURL.String())
+	if err != nil {
+		return nil, fmt.Errorf("Failed to get XML: %v", err)
+	}
+	fmt.Println(xmlString)
+
+	return nil, nil
 }
 
-func (q *Query) BuildQuery(stringQuery string) Query {
-	return Query{}
+// getXML returns the XML body of a given URL string
+func getXML(url string) (string, error) {
+	response, err := http.Get(url)
+	if err != nil {
+		return "", fmt.Errorf("GET error: %v", err)
+	}
+	if response.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("Status error: %v", response.StatusCode)
+	}
+
+	data, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return "", fmt.Errorf("Read body: %v", err)
+	}
+
+	fmt.Println(string(data))
+
+	response.Body.Close()
+
+	return string(data), nil
 }
